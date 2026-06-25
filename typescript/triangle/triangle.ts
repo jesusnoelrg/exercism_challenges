2c6d6d72a7e1928f97eb01eaa12f6f224ac94117
jesusnoelrg <jesusnoelrg.dev@gmail.com>
2026-06-25 13:36:35 -0700

    refactor(triangle.ts): Use isNotTriangle to check all triangles.

--- typescript/triangle/triangle.ts
+++ typescript/triangle/triangle.ts
@@ -1,29 +1,27 @@
 export class Triangle {
-  constructor(
-    private readonly a: number,
-    private readonly b: number,
-    private readonly c: number
-  ) {}
+  public sides: number[]
 
-  private sides = (): Array<number> => Array.of(this.a, this.b, this.c); 
-  private isNotTriangle = (): boolean => {
-    const [lado_a, lado_b, largo] = this.sides().sort((x, y) => x - y);
-    return lado_a + lado_b <= largo || this.sides().some(x => x === 0);
+  constructor(
+    ...sides: number[]
+  ) {
+    this.sides = sides
   }
-  private sidesDuplicates = (): number => new Set(this.sides()).size;
+
+  private isNotTriangle = (): boolean => {
+    const [lado_a, lado_b, largo] = this.sides.sort((x, y) => x - y);
+    return lado_a + lado_b <= largo;
+  }
+  private sidesDuplicates = (): number => new Set(this.sides).size;
 
   get isEquilateral(): boolean {
     return !this.isNotTriangle() && this.sidesDuplicates() === 1
   }
 
   get isIsosceles(): boolean {
-    if (this.isNotTriangle() && this.sidesDuplicates() !== 2) return false;
-    let maxN = this.sides().reduce((max, n) => max > n ? max : n);
-
-    return this.sides().filter(x => x !== maxN).length === 1 || this.sidesDuplicates() === 1;
+    return !this.isNotTriangle() && this.sidesDuplicates() <= 2;
   }
 
   get isScalene(): boolean {
-    return this.sidesDuplicates() === 3 && !this.isNotTriangle();
+    return !this.isNotTriangle() && this.sidesDuplicates() === 3;
   }
 }
