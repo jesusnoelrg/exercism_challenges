a3f2955ab2380cc1ea845e71f049e37e16b49ae3
jesusnoelrg <jesusnoelrg.dev@gmail.com>
2026-07-02 15:02:30 -0700

    refactor(etl.ts): use reduce to avoid using a mutable variable.

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -1,11 +1,6 @@
 export function transform(old: Record<number, string[]>): Record<string, number> {
-  let newForm: Record<string, number> = {}; 
-
-  Object.entries(old).forEach(([key, values]) => {
-    for(let v of values.map(x => x.toLowerCase()) as string[]){
-      newForm[v] = parseInt(key);
-    }
-  })
-
-  return newForm;
+  return Object.entries(old).reduce((acc, [key, values]) => {
+    for(let v of values.map(x => x.toLowerCase()) as string[]) acc[v] = parseInt(key);
+    return acc
+  }, {} as Record<string, number>)
 }
