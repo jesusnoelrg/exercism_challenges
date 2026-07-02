72c729b70b6b71a7210dd06d5c5614aa0b5fc908
jesusnoelrg <jesusnoelrg.dev@gmail.com>
2026-07-02 15:04:17 -0700

    refactor(etl.ts): delete the map to 'values' to avoid redundancy in loops.

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -1,6 +1,6 @@
 export function transform(old: Record<number, string[]>): Record<string, number> {
   return Object.entries(old).reduce((acc, [key, values]) => {
-    for(let v of values.map(x => x.toLowerCase()) as string[]) acc[v] = parseInt(key);
+    for(let v of values) acc[v.toLowerCase()] = parseInt(key);
     return acc
   }, {} as Record<string, number>)
 }
